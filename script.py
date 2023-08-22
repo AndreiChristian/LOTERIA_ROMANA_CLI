63eb8cf754e59ed1ce4e2a0f8f1544240dd67c78
Andrei Christian <andreichristiannetoiu@gmail.com>
2023-08-22 03:02:01 +0300

    implemented ascii art

--- script.py
+++ script.py
@@ -2,6 +2,16 @@ import random
 import time
 import os
 
+
+def print_loteria_ascii_art():
+    print("  _           _            _         _____                                   ")
+    print(" | |         | |          (_)       |  __ \                                  ")
+    print(" | |     ___ | |_ ___ _ __ _  __ _  | |__) |___  _ __ ___   __ _ _ __   __ _ ")
+    print(" | |    / _ \| __/ _ \ '__| |/ _` | |  _  // _ \| '_ ` _ \ / _` | '_ \ / _` |")
+    print(" | |___| (_) | ||  __/ |  | | (_| | | | \ \ (_) | | | | | | (_| | | | | (_| |")
+    print(" |______\___/ \__\___|_|  |_|\__,_| |_|  \_\___/|_| |_| |_|\__,_|_| |_|\__,_|")
+
+
 class LoteriaRomana:
     def __init__(self):
         self.participanti = ["Rafa", "Cecilia", "Alex", "Horatiu", "Ivona", "Andrei", "Nicolae"]
@@ -14,10 +24,8 @@ class LoteriaRomana:
             self.clear_console()
             print("\033[1;36;40mToti participantii au fost selectati. Resetare...\033[0m")
             time.sleep(1.5)
-        
         # Select a participant who hasn't won yet
         participant = random.choice([s for s in self.participanti if s not in self.castigatori])
-        
         self.castigatori.append(participant)
         return participant
 
@@ -27,14 +35,15 @@ class LoteriaRomana:
     def afisare_castigator(self, nume):
         print("\033[1;34;40m-----------------------------------------------------\033[0m")
         print("\033[1;34;40m|                                                   |\033[0m")
-        print("\033[1;34;40m|    \033[1;37;40mMarele castigator:\033[1;34;40m                           |\033[0m")
+        print("\033[1;34;40m|    \033[1;37;40mMarele castigator: \033[1;34;40m                            |\033[0m")
         print("\033[1;34;40m|     \033[1;37;40m{}\033[1;34;40m                          |\033[0m".format(nume.center(20)))
         print("\033[1;34;40m|                                                   |\033[0m")
         print("\033[1;34;40m-----------------------------------------------------\033[0m")
 
-
     def run(self):
         self.clear_console()
+        print_loteria_ascii_art()
+
         print("\033[1;36;40mBine ati venit la Loteria Romana!\033[0m")
         while True:
             input("\033[1;96;40mApasa ENTER pentru a selecta marele castigator...\033[0m")
@@ -47,3 +56,4 @@ class LoteriaRomana:
 if __name__ == "__main__":
     loterie = LoteriaRomana()
     loterie.run()
+
